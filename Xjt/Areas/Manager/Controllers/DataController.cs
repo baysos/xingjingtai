e76d41a4dead44c4d03096201a93d30744fa867b
zhouzhou <17707002@qq.com>
2018-12-07 16:48:09 +0800

    更新

--- Xjt/Areas/Manager/Controllers/DataController.cs
+++ Xjt/Areas/Manager/Controllers/DataController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Xjt.Areas.Manager.Models;
 using Xjt.Common;
 
 namespace Xjt.Areas.Manager.Controllers
@@ -15,11 +18,22 @@ namespace Xjt.Areas.Manager.Controllers
             return View();
         }
 
-        public ActionResult Login()
+        public ActionResult Login(string name, string pass)
         {
-            var p = "xjt123456";
-            ViewBag.pass = SecurityHelper.CreateMd5Str(p + ConstValue.PassSalt);
+            if (Request.IsAjaxRequest())
+            {
+                Response.Redirect("/Manager/data/index");
+            }
             return View();
+        }
+
+        private JsonResult Result(ResultType type, object obj)
+        {
+            var jsonResult = new JsonResult();
+            jsonResult.ContentType = "application/json";
+            jsonResult.Data = new { code = (int)type, data = JsonConvert.SerializeObject(obj) };
+
+            return jsonResult;
         }
     }
 }
---
+++ Xjt/Areas/Manager/Models/ResultType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Xjt.Areas.Manager.Models
+{
+    public enum ResultType
+    {
+        Success = 200,
+        Exception = 500
+    }
+}
